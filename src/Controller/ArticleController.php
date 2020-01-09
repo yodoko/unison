733afd88bc7b54ad5b981f7e9760299bd22c1dca
sitedevente <martinjarod17@gmail.com>
2020-01-09 19:36:24 +0100

    Added path to product/* and dummy data to ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -31,22 +31,20 @@ class ArticleController extends AbstractController
         */
         
         $product_array = [
-            [   
+            [
+                'id' => 0,   
                 'product_name' => 'Nom du produit',
                 'image_name' => 'blanc.jpg',
                 'price' => 35.50 ,
             ],
             [   
-                'product_name' => 'Nom du produit',
-                'image_name' => 'noir.jpg',
-                'price' => 35.50 ,
-            ],
-            [   
+                'id' => 1,
                 'product_name' => 'Chemise tendance',
                 'image_name' => 'noir.jpg',
                 'price' => 53.00 ,
             ],
             [   
+                'id' => 2,
                 'product_name' => 'Contre le racisme',
                 'image_name' => 'jaune.jpg',
                 'price' => 15.00 ,
@@ -59,6 +57,115 @@ class ArticleController extends AbstractController
         ];
         
         return $this->render('product/index.html.twig', $options);
+    }
+
+    /** 
+     * @Route("/article/voir/{id}", name="app_article_view")
+     * @param EntityManagerInterface $entityManager
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function view(int $id, EntityManagerInterface $entityManager) {
+        
+        $charity_array = [
+            [
+                'id' => 0,
+                'name' => 'WWF',
+                'image_name' => 'logo0.png',
+            ],
+            [
+                'id' => 1,
+                'name' => 'Les restaurants du cœur',
+                'image_name' => 'logo1.png',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Secours populaire français',
+                'image_name' => 'logo2.png',
+            ],
+            [
+                'id' => 3,
+                'name' => 'UNICEF',
+                'image_name' => 'logo3.png',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Action contre La Faim',
+                'image_name' => 'logo4.png',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Sea Shepherd Conservation Society',
+                'image_name' => 'logo5.png',
+            ],
+            [
+                'id' => 6,
+                'name' => 'Croix-Rouge française',
+                'image_name' => 'logo6.png',
+            ],
+            [
+                'id' => 7,
+                'name' => 'Médecins sans frontières',
+                'image_name' => 'logo7.png',
+            ],
+            [
+                'id' => 8,
+                'name' => '30 Millions d\'Amis',
+                'image_name' => 'logo8.png',
+            ],
+            [
+                'id' => 9,
+                'name' => 'Institut Pasteur',
+                'image_name' => 'logo9.png',
+            ],
+        ];
+
+        $name ;
+        $product_array = [];
+        
+        if($id === 0){
+            $name = 'Nom du produit';
+
+            array_push($product_array, [   
+                'color' => 'blanc',
+                'image_name' => 'blanc.jpg',
+            ]);
+            
+            array_push($product_array, [   
+                'color' => 'noir',
+                'image_name' => 'noir.jpg',
+            ]);
+
+            array_push($product_array, [
+                'color' => 'pas d\'image',
+                'image_name' => 'aucune'
+            ]);
+            
+        }else if($id === 1) {
+            $name = 'Chemise tendance';
+
+            array_push($product_array, [   
+                'id' => 1,
+                'image_name' => 'noir.jpg',
+                'price' => 53.00 ,
+            ]);
+        }else {
+            $name = 'Contre le racisme';
+
+            array_push($product_array, [   
+                'id' => 2,
+                'image_name' => 'jaune.jpg',
+                'price' => 15.00 ,
+            ]);
+        }
+
+        $options = [
+            'authentified' => false,
+            'name' => $name,
+            'product_array' => $product_array,
+            'charity_array' => $charity_array,
+        ];
+        
+        return $this->render('product/show.html.twig', $options);
     }
 
     /**

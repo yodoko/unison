212ae880f59d9b18a1452ed9b613a1611aadd941
sitedevente <martinjarod17@gmail.com>
2020-01-09 22:47:26 +0100

    Fixed issue with error.js

--- public/js/error.js
+++ public/js/error.js
@@ -1,14 +1,9 @@
-
-function loadDefaultImg() {
+function loadDefaultImg(noImage) {
     var imgArray = document.querySelectorAll('img')
     for (var i = 0; i < imgArray.length; ++i) {
         var img = imgArray[i];
         img.addEventListener('error', function () {
-            this.src = '../images/produit/no_image.jpg'
+            this.src = noImage
         }.bind(img))
     }
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    loadDefaultImg()
-})

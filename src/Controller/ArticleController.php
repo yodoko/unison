bf47b85d03a2ef634613237ea3024ff37740b481
Adnyropast <dealamu134579@gmail.com>
2020-01-09 15:23:30 +0100

    Updated ArticleController.php migrated product dummy data sets from temp-css

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -18,11 +18,47 @@ class ArticleController extends AbstractController
      */
     public function list(EntityManagerInterface $entityManager)
     {
-        $articles =$entityManager->getRepository(Article::class)->findAll();
-
-        return $this->render('product/index.html.twig', [
-            'articles' => $articles
-        ]);
+        // $articles =$entityManager->getRepository(Article::class)->findAll();
+        
+        // return $this->render('product/index.html.twig', [
+            // 'articles' => $articles
+        // ]);
+        
+        /** 
+         * Here we suppose that the back end find every first image for N products. 
+         * Their id(s) for the link toward the product info page 
+         * The name, price and category
+        */
+        
+        $product_array = [
+            [   
+                'product_name' => 'Nom du produit',
+                'image_name' => 'blanc.jpg',
+                'price' => 35.50 ,
+            ],
+            [   
+                'product_name' => 'Nom du produit',
+                'image_name' => 'noir.jpg',
+                'price' => 35.50 ,
+            ],
+            [   
+                'product_name' => 'Chemise tendance',
+                'image_name' => 'noir.jpg',
+                'price' => 53.00 ,
+            ],
+            [   
+                'product_name' => 'Contre le racisme',
+                'image_name' => 'jaune.jpg',
+                'price' => 15.00 ,
+            ],
+        ];
+        
+        $options = [
+            'authentified' => false,
+            'product_array' => $product_array,
+        ];
+        
+        return $this->render('product/index.html.twig', $options);
     }
 
     /**

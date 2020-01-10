f50c1dbe5f96b64df1a3b02aede4383a45e097bb
sitedevente <martinjarod17@gmail.com>
2020-01-10 03:52:30 +0100

    Finished circular navigation

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -11,8 +11,28 @@ class BasketController extends AbstractController
     /**
      * @Route("/basket", name="app_basket")
      */
-    public function basket()
+    public function index()
+    {
+        // if authentified 
+        return $this->render('cart/index.html.twig');
+        // else
+        // return $this->render('cart/connection.html.twig');
+    }
+
+    /**
+     * @Route("/basket/add", name="app_basket_add")
+     */
+    public function add()
+    {
+        return $this->redirectToRoute('app_articles');
+    }
+
+     /**
+     * @Route("/basket/confirm", name="app_basket_confirm")
+     */
+    public function confirm()
     {
         return $this->render('cart/index.html.twig');
     }
+
 }

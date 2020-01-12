29025ed39523ea398889a8537dfe04b1ee21d321
sitedevente <martinjarod17@gmail.com>
2020-01-12 02:59:14 +0100

    Updated RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,61 +16,42 @@ class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param GuardAuthenticatorHandler $guardHandler
-     * @param AppCustomAuthenticator $authenticator
-     * @return Response
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, AppCustomAuthenticator $authenticator): Response
     {
-//        $user = new User();
-//        $form = $this->createForm(RegistrationFormType::class, $user);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            // encode the plain password
-//            $user->setPassword(
-//                $passwordEncoder->encodePassword(
-//                    $user,
-//                    $form->get('plainPassword')->getData()
-//                )
-//            );
-//
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->persist($user);
-//            $entityManager->flush();
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        $condition = $form->isSubmitted();
+//      $condition &= $form->isValid();
+
+        if ($condition) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
             // do anything else you need here, like send an email
-//
-//            return $guardHandler->authenticateUserAndHandleSuccess(
-//                $user,
-//                $request,
-//                $authenticator,
-//                'main' // firewall name in security.yaml
-//            );
-//        }
 
+            return $this->redirectToRoute('home_index');
 
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
 
-        return $this->render('account/add.html.twig', [
-            //'registrationForm' => $form->createView(),
-        ]);
+        }
+
+        return $this->render('registration/add.html.twig');
     }
-
-    /**
-     * @Route("/account_index", name="app_account_index")
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param GuardAuthenticatorHandler $guardHandler
-     * @param AppCustomAuthenticator $authenticator
-     * @return Response
-     */
-    public function add(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, AppCustomAuthenticator $authenticator): Response
-    {
-
-
-        return $this->render('account/index.html.twig');
-    }
-
 }
